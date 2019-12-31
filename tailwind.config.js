621572bcbaf5cf6643f921108d225bdfc99391fe
James MacIvor <jmacivor@steemer.com>
2019-12-31 15:27:45 -0500

    added nav component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      maxHeight: {
+        none: '0'
+      }
+    }
+  },
   variants: {},
-  plugins: []
+  plugins: [require('tailwindcss-transitions')()]
 }
