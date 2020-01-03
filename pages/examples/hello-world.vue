2ca03b8dadadcdb1bba32cd5a27b57d800e022cd
James MacIvor <jmacivor@steemer.com>
2020-01-03 16:01:44 -0500

    fixed issue with routing on example pages

--- pages/examples/hello-world.vue
+++ pages/examples/hello-world.vue
@@ -1,3 +1,21 @@
 <template>
-    
+    <div class="ml-10 p-10 pt-0">   
+        <div class="lg:flex lg:justify-center w-full">      
+            <div class="lg:w-2/3 mt-6">
+                <section>
+                    <div class="lg:ml-10">
+                        <h1 class="mt-6 h1 leading-tight text-center">Hello World</h1>                      
+                        <p class="p-8">
+                            While this section will serve to show how to do something with Feather Http, we'll also be going over some of the core concepts necessary to understand what Feather Http is attempting to do and how it works.
+                            We'll be breaking down these core concepts: 
+                            <ol class="ml-12">
+                                <li>WebApplicationHost</li>
+                                <li>Builder</li>
+                            </ol>
+                        </p>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
 </template>
