c7af4fb13bac461c1e4856407b3ec3b4a2b2e202
James MacIvor <jmacivor@steemer.com>
2020-01-04 15:50:57 -0500

    fixed hello world mobile styling

--- pages/examples/hello-world.vue
+++ pages/examples/hello-world.vue
@@ -5,21 +5,22 @@
                 <section>
                     <div class="lg:ml-10">
                         <h1 class="mt-6 h1 leading-tight text-center">Hello World</h1>                      
-                        <p class="p-8">
+                        <p class="pb-0">
                             While this section will serve to show how to do something with Feather Http, we'll also be going over some of the core concepts necessary to understand what Feather Http is attempting to do and how it works.
                             We'll be breaking down these core concepts: 
-                            <ol class="ml-12">                                
+                            <ol>                                
                                 <li>WebApplicationHostBuilder</li>
                                 <li>WebApplicationHost</li>
                                 <li>WebHostEnvironment</li>                                
                             </ol>
                         </p>
-                        <p class="p-8">But first, here is the code to get your project up, running and saying "Hello World!" You can add this to the program.cs</p>
+                        <p class="pt-0">But first, here is the code to get your project up, running and saying "Hello World!" You can add this to the program.cs</p>
                         <div>
-                            <prism class="" :code="helloWorldCode" language="csharp">
+                            <prism class="overflow-scroll lg:overflow-hidden" :code="helloWorldCode" language="csharp">
                             </prism>
                         </div>                        
-                        <p class="p-8">Now you can run this by typing <span class="code">dotnet run.</span> You can then navigate to <a class="underline" href="http://localhost:5000">http://localhost:5000</a> view the output.</p>
+                        <p>Now you can run this by typing <span class="code">dotnet run.</span> You can then navigate to <a class="underline" href="http://localhost:5000">http://localhost:5000</a> view the output.</p>
+                        <p>If you installed the template from getting started, then you already have this code.</p>
                     </div>
                 </section>
             </div>
